73222a831c3f02ab7963d708e6303da7ee9860d5
sibylle06 <72032117+sibylle06@users.noreply.github.com>
2021-01-05 18:45:59 +0100

    Update MangleMA.py

--- appPhysique/MangleMA.py
+++ appPhysique/MangleMA.py
@@ -13,21 +13,22 @@ def deuxpoints():
     print(b)
     print("y=", m,"x+",b)
 
-def unameruncapunedistance():
-    Xamer = float(input("Ta position x de l amer"))
-    Yamer = float(input("Ta position y de l amer"))
-    cap = float(input("angle Beta entre l'axe du Nord et la droite MA (en degré)"))
-    distance=float(input('quelle distance parcours le bateau ?'))
-
+def unameruncapunedistance(longitude,latitude,cap,distance):
+    R = 6378137.0
+    Xamer = R * cos(longitude) * sin(latitude)
+    Yamer = R * sin(longitude) * sin(latitude)
     m = tan(pi/2 - cap*pi/180)
     b=Yamer-m*Xamer
-    print("y=", m,"x+",b)
+    print("y=", m, "x+", b)
     x=sin(cap*pi/180)*distance
     y=m*x+b
-    print(x,y)
+    print(x, y)
 
 
-
-unameruncapunedistance()
+longitude = float(input("Ta position x de l amer"))
+latitude = float(input("Ta position y de l amer"))
+cap = float(input("angle Beta entre l'axe du Nord et la droite MA (en degré)"))
+distance=float(input('quelle distance parcours le bateau ?'))
+unameruncapunedistance(longitude,latitude,cap,distance)
 
 
